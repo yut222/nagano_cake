c080996a74cf24c0bef403966f8897cfff3db452
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2023-02-10 19:24:03 +0000

    Add admin/genres ジャンル登録

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,9 +1,27 @@
 class Admin::GenresController < ApplicationController
   def index
-    @genres = Genre.page(params[:page]).per(8)
+    @genres = Genre.page(params[:page]).per(10)
     @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+       flash[:notice] = "ジャンルを追加しました"
+       redirect_to admin_genres_path
+    else
+      @genres = Genre.all.page(params[:page]).per(10)
+      render :index and return
+    end
   end
 
   def edit
   end
+
+    private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
 end
