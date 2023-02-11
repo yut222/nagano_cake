dc3f3b249c454ad313d7485f8a70dfde8fe867ae
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2023-02-11 12:00:38 +0000

    Update admin 管理者トップページ

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,9 @@
 class Admin::HomesController < ApplicationController
+
   def top
+    @params = params[:id]
+    @order = Order.where(customer_id: @params).page(params[:page]).per(10).order(created_at: :desc)
+    @orders = Order.page(params[:page]).per(10).order(created_at: :desc)
   end
+
 end
