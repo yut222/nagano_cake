dfe922ed5b28064f52dccbf2e2ed403b6a75c8d5
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2023-02-15 15:21:46 +0000

    Add orders/confirm 注文情報確認(エラー中)

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -10,7 +10,43 @@ class Public::OrdersController < ApplicationController
     @addresses = @customer.addresses
   end
 
+  # 購入確認画面
   def confirm
+    @cart_items = current_customer.cart_items
+
+    @order = Order.new(
+      customer: current_customer,
+      payment_method: params[:order][:payment_method])
+
+    # total_paymentに請求額を入れる billingはhelperで定義
+    @order.total_payment = billing(@order)
+
+    # my_addressに1が入っていれば（自宅）
+    if params[:order][:my_address] == "1"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = full_name(current_customer)
+
+    # my_addressに2が入っていれば（配送先一覧）
+    elsif params[:order][:my_address] == "2"
+      ship = Address.find(params[:order][:address_id])
+      @order.postal_code = ship.postal_code
+      @order.address = ship.address
+      @order.name = ship.name
+
+    # my_addressに3が入っていれば(新配送先)
+    elsif params[:order][:my_address] == "3"
+      @order.postal_code = params[:order][:postal_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
+      @ship = "1"
+
+    # 有効かどうかの確認
+      unless @order.valid? == true
+        @addresses = Address.where(customer: current_customer)
+        render :new
+      end
+    end
   end
 
   def thanks
@@ -21,4 +57,15 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+
+    private
+
+  def order_params
+    params.require(:order).permit(:postal_code, :address, :name)
+  end
+
+  def address_params
+    params.require(:order).permit(:postal_code, :address, :name)
+  end
+
 end
