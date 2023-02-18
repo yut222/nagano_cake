b25ba4c322f2ad60f65f9e43a6c34c0103fb80a8
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2023-02-18 13:29:19 +0000

    Add orders 注文履歴一覧

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,6 +1,6 @@
 class Public::OrdersController < ApplicationController
 
-  before_action :to_log, only: [:show]
+  # before_action :to_log, only: [:show]
   before_action :authenticate_customer!
 
   def new
@@ -13,28 +13,21 @@ class Public::OrdersController < ApplicationController
 # 購入を確定します
 def create # Order に情報を保存します
   cart_items = current_customer.cart_items.all
-# ログインユーザーのカートアイテムをすべて取り出して cart_items に入れます
-  @order = current_customer.orders.new(order_params)
-# 渡ってきた値を @order に入れます
-  if @order.save
-# ここに至るまでの間にチェックは済ませていますが、念の為IF文で分岐させています
-    cart_items.each do |cart|
-# 取り出したカートアイテムの数繰り返します
-# order_detail にも一緒にデータを保存する必要があるのでここで保存します
+  @order = current_customer.orders.new(order_params)  # ログインユーザーのカートアイテムをすべて取り出して cart_items に入れます
+  if @order.save  # 渡ってきた値を @order に入れます
+    cart_items.each do |cart|  # ここに至るまでの間にチェックは済ませていますが、念の為IF文で分岐させています
+    # 取り出したカートアイテムの数繰り返します  # order_detail にも一緒にデータを保存する必要があるのでここで保存します
       order_detail = OrderDetail.new
       order_detail.item_id = cart.item.id
       order_detail.price = cart.item.price
       order_detail.order_id = @order.id
       order_detail.amount = cart.amount
-# 購入が完了したらカート情報は削除するのでこちらに保存します
-      order_detail.price = cart.item.price
-# カート情報を削除するので item との紐付けが切れる前に保存します
-      order_detail.save
+      order_detail.price = cart.item.price  # 購入が完了したらカート情報は削除するのでこちらに保存します
+      order_detail.save  # カート情報を削除するので item との紐付けが切れる前に保存します
     end
-    cart_items.destroy_all    # 購入後はカート内商品削除
-    redirect_to orders_thanks_path
 
-# ユーザーに関連するカートのデータ(購入したデータ)をすべて削除します(カートを空にする)
+    cart_items.destroy_all    # 購入後はカート内商品削除(カートを空にする)
+    redirect_to orders_thanks_path
   else
     render :new
   end
@@ -75,7 +68,7 @@ end
   end
 
   def index
-
+    @orders = current_customer.orders
   end
 
   def show
