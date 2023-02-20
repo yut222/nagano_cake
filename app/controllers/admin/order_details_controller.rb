ecf8e517568e6975d13f703d3aa584ee2bc5527a
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2023-02-20 11:26:27 +0000

    Add enum 記述(修正完了)

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -5,17 +5,16 @@ class Admin::OrderDetailsController < ApplicationController
     @order_detail.update(order_detail_params)
     @order = @order_detail.order
 
-
-
-		case order_detail.making_status
-		 when "製作中"
-				order_detail.order.update(order_status: "making")
-		 when "製作完了"
-			if order_detail.order.order_details.all?{|order_detail| order_detail.making_status == "make_done"}
-				order_detail.order.update(order_status: "delivery")  # 発送準備中
-			end
-		end
-		redirect_to admins_order_path(order_detail.order.id)
+    # 制作ステータスを変更したら、入金ステータスも変わるようにする処理
+    case @order_detail.making_status
+      when "製作中"
+        @order_detail.order.update(order_status: "making")  # 製作中
+      when "製作完了"
+        if @order_detail.order.order_details.all?{|order_detail| order_detail.making_status == "make_done"}
+          @order_detail.order.update(order_status: "delivery")  # 発送準備中
+        end
+    end
+      redirect_to admin_order_path(@order_detail.order.id)
   end
 
     private
